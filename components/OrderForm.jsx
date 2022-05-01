13cd099e4fc326b8d0b0c38cc35da236d843b4ba
Alex Tabula <67907195+TabulaWeb@users.noreply.github.com>
2022-05-02 00:51:36 +0300

    form

--- components/OrderForm.jsx
+++ components/OrderForm.jsx
@@ -1,6 +1,14 @@
 import styles from '../styles/OrderForm.module.css';
+import {useState} from 'react'
 
 const OrderForm = () => {
+    const [check, setCheck] = useState(false);
+
+    const getCheck = (check) => {
+        setCheck(check)
+        console.log(check)
+    }
+
     return (
         <section className='order'>
             <div className="container">
@@ -8,10 +16,23 @@ const OrderForm = () => {
                     <h2 className={styles.orderFormTitle}>Оформить заявку онлайн</h2>
 
                     <div className={styles.orderFormContainer}>
-                        <div className={styles.formSwitch}>
-                            <p className='formSwitchText active'>Для себя</p>
-                            <p className='formSwitchText'>Для бизнеса</p>
+                        <div className={check ? 'formSwitch active' : 'formSwitch'}>
+                            <p className={!check ? 'formSwitchText active' : 'formSwitchText'} onClick={() => getCheck(false)}>Для себя</p>
+                            <p className={check ? 'formSwitchText active' : 'formSwitchText'} onClick={() => getCheck(true)}>Для бизнеса</p>
                         </div>
+
+                        <form>
+                            <input className={styles.orderFormInput} type="text" name='name' placeholder='Полные ФИО' />
+                            <input className={styles.orderFormInput} type="mail" name='mail' placeholder='Электронная почта' />
+                            <input className={styles.orderFormInput} type="phone" name='phone' placeholder='Номер телефона' />
+                            <label className={styles.orderFormCheckbox}>
+                                <input className={styles.orderFormCheckboxInput} type="checkbox" />
+                                <span className={styles.orderFormCheckboxCustom}></span>
+                                <p className={styles.orderFormCheckboxText}>Указывая свои персональные данные, вы даете согласие на их обработку</p>
+                            </label>
+                            
+                            <button className={styles.orderFormButton}>Оформить заявку</button>
+                        </form>
                     </div>
                 </div>
             </div>
