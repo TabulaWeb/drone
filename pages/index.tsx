ad27770ddd4c9ef2e377929d653d0401516abb51
Alex Tabula <67907195+TabulaWeb@users.noreply.github.com>
2022-05-02 13:53:33 +0300

    work

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,13 @@ import type { NextPage } from 'next';
 import styles from '../styles/Home.module.css';
 import OrderForm from '../components/OrderForm';
 import Reviews from '../components/Reviews';
+import Work from '../components/Work';
+
 
 const Home: NextPage = () => {
   return (
     <>
+      <Work />
       <Reviews />
       <OrderForm />
     </>
